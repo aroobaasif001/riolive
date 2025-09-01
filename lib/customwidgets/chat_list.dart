823b9f3d02194738127619449ee8dc566a17a0e1
Maaz Ahmad <129156034+maaz335@users.noreply.github.com>
2025-09-01 18:07:09 +0500

    maaz update 01-09-2025 part 5

---
+++ lib/customwidgets/chat_list.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+import '../controller/party_room_controller.dart';
+import 'chat_bubble.dart';
+
+class ChatList extends GetView<PartyRoomController> {
+  const ChatList();
+  @override
+  Widget build(BuildContext context) {
+    final screenW = Get.width;
+    return Obx(
+      () => ListView.builder(
+        itemCount: controller.messages.length,
+        padding: EdgeInsets.zero,
+        itemBuilder: (_, i) {
+          final m = controller.messages[i];
+          return Padding(
+            padding: const EdgeInsets.only(bottom: 10),
+            child: ChatBubble(m, maxWidth: screenW * 0.68),
+          );
+        },
+      ),
+    );
+  }
+}
