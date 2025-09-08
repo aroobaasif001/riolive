0e0ef6c0b602a23b861ef0c79fde74416e44c9a9
Khattak11 <touseefktk2019@gmail.com>
2025-09-07 22:28:54 -0700

    live calling +  private calls + filters

--- lib/customwidgets/custom_gradient_button.dart
+++ lib/customwidgets/custom_gradient_button.dart
@@ -13,6 +13,8 @@ class CustomGradientButton extends StatelessWidget {
   final FontWeight fontWeight;
   final List<Color> gradientColors;
   final Color textColor; // Added this to customize the text color
+  final Alignment? begin ; // Added this to customize the text color
+  final Alignment? end; // Added this to customize the text color
 
   const CustomGradientButton({
     Key? key,
@@ -25,7 +27,7 @@ class CustomGradientButton extends StatelessWidget {
     this.fontSize = 14,
     this.fontWeight = FontWeight.bold,
     this.gradientColors = const [Color(0xFF8EC2FB), Color(0xFFE496FF)],
-    this.textColor = Colors.white, // Default text color is white
+    this.textColor = Colors.white, this.begin,  this.end, // Default text color is white
   }) : super(key: key);
 
   @override
